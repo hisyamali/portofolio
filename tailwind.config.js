d697b3b3573b602be9376f850f95bdd851d39a36
hisyamali <hisyamaliwafi@gmail.com>
2024-10-04 16:04:51 +0700

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,25 @@
+// import defaultTheme from 'tailwindcss/defaultTheme';
+// import forms from '@tailwindcss/forms';
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+//         './storage/framework/views/*.php',
+//         './resources/views/**/*.blade.php',
+//         './resources/js/**/*.vue',
+//     ],
+
+//     theme: {
+//         extend: {
+//             fontFamily: {
+//                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+//             },
+//         },
+//     },
+
+//     plugins: [forms],
+// };
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
@@ -8,6 +30,7 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -18,5 +41,6 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [forms, require('flowbite/plugin')],
+    darkMode: 'class',
 };
