b37a4b76630af45c9274a00673c1651cd6112ebb
hisyamali <hisyamaliwafi@gmail.com>
2024-10-22 14:39:00 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             // ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
@@ -18,4 +18,11 @@ export default defineConfig({
             },
         }),
     ],
+    // Ensure base is relative to load assets correctly in production
+    base: process.env.APP_URL ? new URL(process.env.APP_URL).origin + "/" : "/",
+    server: {
+        // Force HTTPS in development (optional, based on your setup)
+        https: true,
+        host: "0.0.0.0", // Ensure server is accessible over network
+    },
 });
