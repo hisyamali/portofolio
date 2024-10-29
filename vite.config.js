ef92116a577a7480718108068e8a5f145788856e
hisyamali <hisyamaliwafi@gmail.com>
2024-10-29 10:58:25 +0700

    delete comment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
-const isLocal = process.env.APP_ENV === "local";
-const base = isLocal ? "/" : process.env.APP_URL ? new URL(process.env.APP_URL).origin + "/" : "/";
-
 export default defineConfig({
     plugins: [
         laravel({
@@ -21,15 +18,4 @@ export default defineConfig({
             },
         }),
     ],
-    // Ensure base is relative to load assets correctly in production
-    // base,
-    // ...(isLocal
-    //     ? {} // If local, do not configure the server
-    //     : {
-    //           server: {
-    //               // Force HTTPS in non-local environments (optional)
-    //               https: true,
-    //               host: "0.0.0.0", // Ensure server is accessible over the network
-    //           },
-    //       }),
 });
